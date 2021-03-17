c073e76e2edceb07080b1c9d29027d22253a7a7a
ManojRasika119 <56877427+ManojRasika119@users.noreply.github.com>
2021-03-17 19:25:38 +0100

    1. started writing the code, but still have no knowledge of using array. 2. I just defined variables and try out with an example code.

--- Documents/NetBeansProjects/classproject1/src/matrix/matrix.java
+++ Documents/NetBeansProjects/classproject1/src/matrix/matrix.java
@@ -15,7 +15,38 @@ public class matrix {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int M, N, O, P; //rows and columns
+        int [][] dataA; //MxN array
+        int [][] dataB; //PxQ array
+        
+        // down below is totally experiment not ready yet
+        
+        // create and return a random 
+        //M-by-N matrix with random values between -10 and 10
+        
+    
+    public matrix(matrix A) { matrix(int M, int N); }
+        
+   
+    @SuppressWarnings("empty-statement")
+    public static matrix random(int M, int N) {
+        matrix A = new matrix(M, N);
+        for (int  i < M;);
+            for (int  j < N;);
+        int[][] dataA;
+                dataA [i][j] = Math.random();
+        return A;
+            
+        }
+
+    private matrix(int M, int N) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+            
+        }
+        
+        
+    
     
 }
