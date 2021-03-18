30db20933df7db26fa31d29e404b2f0f48594aed
Michael Bazant <bazantm@gmail.com>
2021-03-18 11:39:25 +0100

    Change of project name

---
+++ Documents/NetBeansProjects/MatrixManoj/src/matrix/Matrix.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrix;
+
+/**
+ *
+ * @author Medhavi
+ */
+public class Matrix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int M, N, O, P; //rows and columns
+        int [][] dataA; //MxN array
+        int [][] dataB; //PxQ array
+        
+        // create and return a random 
+        //M-by-N matrix with random values between -10 and 10
+        
+        
+    public matrix(matrix A) { matrix(int M, int N); }
+        
+   
+    
+    public static matrix random(int M, int N) {
+        matrix A = new matrix(M, N);
+        for (int  i < M;);
+            for (int  j < N;);
+        int[][] dataA;
+                dataA [i][j] = Math.random();
+        return A;
+        
+        
+    }
+    
+}
