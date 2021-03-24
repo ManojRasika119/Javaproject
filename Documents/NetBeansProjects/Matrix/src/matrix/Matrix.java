054dbea3da4b1c12084c4f02280ce670b4cb6c8d
ManojRasika119 <01manojrasika10@gmail.com>
2021-03-25 00:10:14 +0100

    Initiated M,N,O,P to have random integer numbers

--- Documents/NetBeansProjects/Matrix/src/matrix/Matrix.java
+++ Documents/NetBeansProjects/Matrix/src/matrix/Matrix.java
@@ -1,41 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Creating two random Metrixes and swapp the elements below the 
+Left diagonal with each other
  */
 package matrix;
 
+
+import java.util.Random;
 /**
  *
  * @author Manoj Rasika
  */
-public class Matrix {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int M, N, O, P; //rows and columns
-        int [][] dataA; //MxN array
-        int [][] dataB; //PxQ array
-        
-        // create and return a random 
-        //M-by-N matrix with random values between -10 and 10
-        
-        
-    public matrix(matrix A) { matrix(int M, int N); }
-        
+public class Matrix {
+ 
    
+    public static void main(String args[] ){
+        Random randA = new Random();
+        int M = randA.nextInt();
+        int N = randA.nextInt();
+   
+        Random randB = new Random();
+        int P = randB.nextInt();
+        int O = randB.nextInt();
     
-    public static matrix  random(int M, int N) {
-        matrix A = new  matrix(M, N);
-        for (int  i < M;);
-            for (int  j < N;);
-        int[][] dataA;
-                dataA [i][j] = Math.random();
-        return A;
         
-        
+    
     }
-    
 }
