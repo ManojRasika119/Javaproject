8a7ec10a570480c550c75127a13df5b2c67fcad7
ManojRasika119 <01manojrasika10@gmail.com>
2021-03-31 01:22:12 +0200

    tried to create elements for the A and B Matrixs

--- Documents/NetBeansProjects/Matrix/src/matrix/Matrix.java
+++ Documents/NetBeansProjects/Matrix/src/matrix/Matrix.java
@@ -15,16 +15,46 @@ public class Matrix {
  
    
     public static void main(String args[] ){
+    }
+        
+    private void random(){
+        //creating random dimentions of A and B Matrixs
         Random randA = new Random();
-        int M = randA.nextInt();
-        int N = randA.nextInt();
+        int rowM = randA.nextInt();
+        int columnN = randA.nextInt();
    
         Random randB = new Random();
-        int P = randB.nextInt();
-        int O = randB.nextInt();
-    
-        //comment
+        int rowO = randB.nextInt();
+        int columnP = randB.nextInt();
         
+       
     
+       
+    }
+    private void Array (int[][] twoDArrayA){
+        
+         //creating random elements for A and B Matrixs
+        Random elimentA = new Random();
+                
+        int [][] twoDArrayA =
+       
+        for  ( int i=0;  -10 < i && i < 10; i = elimentA.nextInt()){ 
+        for  ( int j=0;  -10 < j && j < 10; j = elimentA.nextInt()){
+            
+        }
+            System.out.print(twoDArrayA[i][j] + "  " );
+        }
+        
+        Random elimentB = new Random();
+                
+        int [][] twoDArrayB =
+       
+        for  ( int r=0;  -10 < r && r < 10; r = elimentB.nextInt()){ 
+        for  ( int s=0;  -10 < s && s < 10; s = elimentB.nextInt()){
+            
+        }
+            System.out.print(twoDArrayB[r][s] + "  " );
+        }
+    }
     }
 }
