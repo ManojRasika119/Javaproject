15c6d0012aecc0a5d5e8f2294abe8a2b1d149a36
Michael Bazant <bazantm@gmail.com>
2021-04-01 12:31:20 +0200

    intro to OOP in Matrix example

--- Documents/NetBeansProjects/Matrix/src/matrix/Matrix.java
+++ Documents/NetBeansProjects/Matrix/src/matrix/Matrix.java
@@ -4,57 +4,78 @@ Left diagonal with each other
  */
 package matrix;
 
-
 import java.util.Random;
+
 /**
  *
  * @author Manoj Rasika
  */
 
 public class Matrix {
- 
-   
-    public static void main(String args[] ){
+
+    // instance variables - attributes of objects
+    String name;
+    int[][] elements;
+    
+    // constructors
+    Matrix(String name, int numberOfRows, int numberOfColumns) { // this is constructor
+        this.name = name;
+        elements = new int[numberOfRows][numberOfColumns];
     }
+
+    //methods
+    @Override
+    public String toString() {
+        String text = "";
         
-    private void random(){
+        text += name + "\n";
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements[i].length; j++) {
+                text += String.format("%6d", elements[i][j]);                
+            }
+            text += "\n";
+        }
+        return text;
+    }
+    
+    private void random() {
         //creating random dimentions of A and B Matrixs
         Random randA = new Random();
         int rowM = randA.nextInt();
         int columnN = randA.nextInt();
-   
+
         Random randB = new Random();
         int rowO = randB.nextInt();
         int columnP = randB.nextInt();
-        
-       
-    
-       
+
     }
-    private void Array (int[][] twoDArrayA){
-        
-         //creating random elements for A and B Matrixs
-        Random elimentA = new Random();
-                
-        int [][] twoDArrayA =
-       
-        for  ( int i=0;  -10 < i && i < 10; i = elimentA.nextInt()){ 
-        for  ( int j=0;  -10 < j && j < 10; j = elimentA.nextInt()){
-            
-        }
-            System.out.print(twoDArrayA[i][j] + "  " );
-        }
-        
-        Random elimentB = new Random();
-                
-        int [][] twoDArrayB =
-       
-        for  ( int r=0;  -10 < r && r < 10; r = elimentB.nextInt()){ 
-        for  ( int s=0;  -10 < s && s < 10; s = elimentB.nextInt()){
-            
-        }
-            System.out.print(twoDArrayB[r][s] + "  " );
-        }
-    }
-    }
+
+//    private void Array(int[][] twoDArrayA) {
+//
+//        //creating random elements for A and B Matrixs
+//        Random elimentA = new Random();
+//
+//        int[][] twoDArrayA
+//                = //this is not attribute of object! this is local variable
+//                // of the method
+//       
+//        for (int i = 0; -10 < i && i < 10; i = elimentA.nextInt()) {
+//            for (int j = 0; -10 < j && j < 10; j = elimentA.nextInt()) {
+//
+//            }
+//            System.out.print(twoDArrayA[i][j] + "  ");
+//        }
+//
+//        Random elimentB = new Random();
+//
+//        int[][] twoDArrayB =
+//       
+//        for (int r = 0; -10 < r && r < 10; r = elimentB.nextInt()) {
+//            for (int s = 0; -10 < s && s < 10; s = elimentB.nextInt()) {
+//
+//            }
+//            System.out.print(twoDArrayB[r][s] + "  ");
+//        }
+//    }
+
 }
